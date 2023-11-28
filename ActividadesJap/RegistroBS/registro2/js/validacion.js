ab9b92fb4f56f94d81b4c37b3832ca5c4b442bc5
m-dopazo <mdopazo20@gmail.com>
2023-11-28 14:39:26 -0300

    Cositas

--- ActividadesJap/RegistroBS/registro2/js/validacion.js
+++ ActividadesJap/RegistroBS/registro2/js/validacion.js
@@ -1,19 +1,26 @@
 const FORM = document.getElementsByTagName("form")[0];
 const P1 = document.getElementById("p1");
 const P2 = document.getElementById("p2");
+const TYC = document.getElementById("tyc");
+const BOX = document.getElementById("box");
+const FORM2 = document.getElementsByTagName("form")[1];
 
 //Cuando se envia el form
 FORM.addEventListener("submit", (event)=>{
-    matchPass()
+
+    matchPass();
+
     //Si el form no es valido, no enviar nada.
-    if (!FORM.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      } else {
-        alert("Formulario guardado con exito")
-      }
+    if (FORM.checkValidity() && FORM2.checkValidity()) {
+      alert("Formulario guardado con exito");
+    } else {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+    
     //Añadir la clase .was-validated para que aparezcan los msg de validacion
       FORM.classList.add('was-validated')
+      FORM2.classList.add('was-validated')
 })
 
 //Si se modifica el form, chekear si coinciden las contras
@@ -21,8 +28,23 @@ FORM.addEventListener("input", ()=>{
     matchPass();
 })
 
-//Coinciden las contraseñas?
+BOX.addEventListener("change", ()=>{
+  if (!BOX.checked){
+    BOX.setCustomValidity("No chekeado");
+    TYC.setCustomValidity("MAL")
+  } else {
+    BOX.setCustomValidity("");
+    TYC.setCustomValidity("")
+  };
+  FORM2.classList.add('was-validated');
+})
+
+//Coinciden las contraseñas? 
 function matchPass (){
-    P1.value != P2.value ? P2.setCustomValidity("Las contraseñas no coinciden") : P2.setCustomValidity("");
+  P1.value.length < 8 ? P1.setCustomValidity("Contraseña corta") : P1.setCustomValidity("");
+  P2.value == P1.value && P1.checkValidity() ? P2.setCustomValidity("") : P2.setCustomValidity("No coinciden"); 
+  
 }
+
+
 
